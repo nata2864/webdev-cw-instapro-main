1a2e96ed95772703366ce056c846a403a0d83954
Natalia Bashirova <tac-2014@yandex.ru>
2023-04-30 10:06:13 +0200

    Создает страницу добавления поста

--- components/add-post-page-component.js
+++ components/add-post-page-component.js
@@ -4,8 +4,24 @@ export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
     const appHtml = `
     <div class="page-container">
       <div class="header-container"></div>
-      Cтраница добавления поста
+      <div class="form">
+      <h3 class="form-title">Добавить пост</h3>
+      <div class="form-inputs">
+      <div class="upload-image-conrainer">
+      <div class="upload-image">
+      <label class="file-upload-label secondary-button">
+      <input type="file" class="file-upload-input" style="display:none">
+      Выберите фото
+      </label>
+      </div>
+      </div>
+      <label>
+      Опишите фотографию:
+      <textarea class="input textarea" rows="4"></textarea>
+      </label>
+      </div>
       <button class="button" id="add-button">Добавить</button>
+      </div> 
     </div>
   `;
 
@@ -20,4 +36,17 @@ export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
   };
 
   render();
+
+
+renderHeaderComponent({
+  element: document.querySelector(".header-container"),
+});
+
+for (let userEl of document.querySelectorAll(".post-header")) {
+  userEl.addEventListener("click", () => {
+    goToPage(USER_POSTS_PAGE, {
+      userId: userEl.dataset.userId,
+    });
+  });
+}
 }
